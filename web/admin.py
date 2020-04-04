84bc4885972f9453bca3d05462d256ad92e62fde
jay <johnjohnsonokah@gmail.com>
2020-04-04 09:24:20 +0100

    Add chart to django admin

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,56 @@
-from django.contrib import admin
+import json
 
-# Register your models here.
+from django.contrib import admin
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Count
+from django.db.models.functions import TruncDay
+from django.http import JsonResponse
+from django.urls import path
+
+from .models import EmailSubscriber
+
+
+@admin.register(EmailSubscriber)
+class EmailSubscriberAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "created_at")
+    ordering = ("-created_at",)
+
+    def changelist_view(self, request, extra_context=None):
+        # Aggregate new subscribers per day
+        chart_data = (
+            EmailSubscriber.objects.annotate(date=TruncDay("created_at"))
+            .values("date")
+            .annotate(y=Count("id"))
+            .order_by("-date")
+        )
+
+        # Serialize and attach the chart data to the template context
+        as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+        extra_context = extra_context or {"chart_data": as_json}
+
+        # Call the superclass changelist_view to render the page
+        return super().changelist_view(request, extra_context=extra_context)
+
+
+    def get_urls(self):
+        urls = super().get_urls()
+        extra_urls = [
+            path("chart_data/", self.admin_site.admin_view(self.chart_data_endpoint))
+        ]
+        # NOTE! Our custom urls have to go before the default urls, because they
+        # default ones match anything.
+        return extra_urls + urls
+
+    # JSON endpoint for generating chart data that is used for dynamic loading
+    # via JS.
+    def chart_data_endpoint(self, request):
+        chart_data = self.chart_data()
+        return JsonResponse(list(chart_data), safe=False)
+
+    def chart_data(self):
+        return (
+            EmailSubscriber.objects.annotate(date=TruncDay("created_at"))
+            .values("date")
+            .annotate(y=Count("id"))
+            .order_by("-date")
+        )
